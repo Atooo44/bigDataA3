017e6265ebd7250ce7215ab80037db85bfb3b3f5
AlexandreR4 <alexandre.radin@isen-ouest.yncrea.fr>
2023-06-12 11:00:02 +0200

    conversion string -> POSIXlt

--- test.R
+++ test.R
@@ -1,6 +1,12 @@
-print("Hello")
-print("test")
-
 acc <- read.csv("stat_acc_V3.csv", header=TRUE, sep=";")
-# retourne les valeurs de la colonne
-levels(factor(acc$descr_etat_surf))
+
+# dates deja formatees dans le csv
+acc$date <- as.POSIXlt(acc$date)
+
+# obtenir le type d'une colonne
+class(acc$date)
+
+acc$date
+
+# type de chaque colonne
+str(acc)
