fc92590fb635ff5acc4ea1578886e1247da834cd
AlexandreR4 <alexandre.radin@isen-ouest.yncrea.fr>
2023-06-13 11:19:22 +0200

    visualisation nb accident par tranche heure

--- test3.R
+++ test3.R
@@ -30,7 +30,32 @@ graph + labs(title="Nombre d'accidents selon la gravité",
              y="Nombre d'accidents")
 
 # representation graphique : nb accidents par tranches d'heure
-# voir comment sont les données filtrées : date ou posix
+install.packages("stringr")
+
+library(stringr)
+# recuperation de l'heure avec minute et seconde
+date_decoupe <- str_split(acc$date, " ")
+heure = c()
+for(i in 1:length(date_decoupe)){
+  print(date_decoupe[[i]][2])
+  heure <- append(heure, date_decoupe[[i]][2])
+}
+print(heure)
+# recuperation de l'heure uniquement
+heure_decoupe <- str_split(heure, ":")
+heure2 = c()
+for(i in 1:length(heure_decoupe)){
+  print(heure_decoupe[[i]][1])
+  heure2 <- append(heure2, heure_decoupe[[i]][1])
+}
+print(heure2)
+
+x<-data.frame(table(heure2))
+x
+graph <- ggplot(data=x, aes(x=heure2, y=Freq)) +  geom_bar(stat="identity", fill="steelblue") + theme_minimal() + geom_text(aes(label=Freq), vjust=-0.3, size=3.5)
+graph + labs(title="Nombre d'accidents par tranches d'heure",
+             x="Heure",
+             y="Nombre d'accidents")
 
 # representation graphique : nb accidents par departement (2 premiers chiffres code insee)
 x<-data.frame(table(as.integer(acc$id_code_insee/1000)))
